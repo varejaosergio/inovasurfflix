ddbe6325ad970b3aa70c6a6d9ac7180c70ee15da
varejaosergio <varejaosergio@gmail.com>
2020-08-01 17:36:33 -0300

    Segunda Aula completa

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
+/** Desafio master blaster na descrição
+// Colocar um jogo nessa página: https://www.youtube.com/watch?v=jOAU81jdi-c :) */
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+     <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />       
+      <Route component={() => (<div>Página 404</div>)} />
+     </Switch>
+  </BrowserRouter>,  
   document.getElementById('root')
 );
 
